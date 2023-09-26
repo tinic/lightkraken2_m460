40a97e08c1709564fa3b512286e0f059a6c9bde1
Tinic Uro <tinicuro@gmail.com>
2023-09-25 20:48:10 -0700

    Fix build.

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ mkdir -p lightkraken2_debug
 cd lightkraken2_debug
 cmake -G "$build_type" -DCMAKE_SYSTEM_NAME=Generic -DCMAKE_TOOLCHAIN_FILE=../../cmake/arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug ../..
 cmake --build .
-cd ..
+cd ../..
 
 cd build
 rm -rf lightkraken2_release*
@@ -20,4 +20,4 @@ mkdir -p lightkraken2_release
 cd lightkraken2_release
 cmake -G "$build_type" -DCMAKE_SYSTEM_NAME=Generic -DCMAKE_TOOLCHAIN_FILE=../../cmake/arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release  ../..
 cmake --build .
-cd ..
+cd ../..
