c1696d33e9d27b92bb1ab86bedafa0eee2260374
Tinic Uro <turo@adobe.com>
2023-09-30 01:29:05 -0700

    Move blinking to thread.

--- main.c
+++ main.c
@@ -65,20 +65,12 @@ void UART0_Init(void)
 
 }
 
-void tx_application_define(void *first_unused_memory) 
-{
-}
+static TX_THREAD thread_0 = { 0 } ;
+static TX_BYTE_POOL byte_pool_0 = { 0 };
 
-int main()
-{
+void thread_0_entry(ULONG thread_input) {
     int32_t i = 0;
-    
-    SYS_UnlockReg();
 
-    SYS_Init();
-
-    UART0_Init();
-    
     /* Enable GPIO PH to control LED */
     CLK->AHBCLK0 |= CLK_AHBCLK0_GPHCKEN_Msk;
 
@@ -87,29 +79,45 @@ int main()
     printf("|              Simple Blinky Demo                                  |\n");
     printf("+------------------------------------------------------------------+\n");
 
-    /* Init GPIO for LED toggle */
     LED_INIT();
     LED_YELLOW = 1;
     LED_RED = 0;
     LED_GREEN = 0;
 
-    for(size_t c = 0; c < 2; c++) {
+    while(1) {
+
         LED_YELLOW = 0;
         LED_RED = 1;
         LED_GREEN = 1;
-        CLK_SysTickLongDelay(200000);
+        tx_thread_sleep(20);
         LED_YELLOW = 1;
         LED_RED = 0;
-        LED_GREEN = 1;
-        CLK_SysTickLongDelay(200000);
+        LED_GREEN = 1; 
+        tx_thread_sleep(20);
         LED_YELLOW = 1;
         LED_RED = 1;
         LED_GREEN = 0;
-        CLK_SysTickLongDelay(200000);
+        tx_thread_sleep(20);
+
         printf("Iter %d\n", i++);
     }
+}
 
+void tx_application_define(void *first_unused_memory) 
+{
+    void *pointer = 0;
+    tx_byte_pool_create(&byte_pool_0, "byte pool 0", first_unused_memory, 9120);
+    tx_byte_allocate(&byte_pool_0, &pointer, 1024, TX_NO_WAIT);
+    tx_thread_create(&thread_0, "thread 0", thread_0_entry, 0, pointer, 1024, 1, 1, TX_NO_TIME_SLICE, TX_AUTO_START);
+}
+
+int main()
+{
+    SYS_UnlockReg();
+
+    SYS_Init();
+
+    UART0_Init();
+    
     tx_kernel_enter();
-
-
 }
