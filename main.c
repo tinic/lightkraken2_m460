22095962315d53f8829ed064a797336d7197e2de
Tinic Uro <turo@adobe.com>
2023-09-30 14:11:20 -0700

    Chip not stable at 200Mhz, set to 120Mhz instead. Use PLLFN for ethernet clock.

--- main.c
+++ main.c
@@ -23,6 +23,109 @@
 void SYS_Init(void);
 void UART0_Init(void);
 
+void SYS_Lightkraken_Init() {
+
+    // Enable ICE
+    SYS->GPF_MFP0 &= ~(SYS_GPF_MFP0_PF1MFP_Msk | SYS_GPF_MFP0_PF0MFP_Msk);
+    SYS->GPF_MFP0 |= (SYS_GPF_MFP0_PF1MFP_ICE_CLK | SYS_GPF_MFP0_PF0MFP_ICE_DAT);
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* If the macros do not exist in your project, please refer to the related clk.h in Header folder of the tool package */
+    /* Enable clock source */
+    CLK_EnableXtalRC(CLK_PWRCTL_LIRCEN_Msk|CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for clock source ready */
+    CLK_WaitClockReady(CLK_STATUS_LIRCSTB_Msk|CLK_STATUS_HXTSTB_Msk);
+
+    /* Disable PLL first to avoid unstable when setting PLL */
+    CLK_DisablePLL();
+
+    /* Set PLL frequency */
+    CLK->PLLCTL = (CLK->PLLCTL & ~(0x008FFFFFUL)) | 0x00004212UL;
+
+    /* Waiting for PLL ready */
+    CLK_WaitClockReady(CLK_STATUS_PLLSTB_Msk);
+
+    /* Disable PLLFN first to avoid unstable when setting PLLFN */
+    CLK->PLLCTL |= CLK_PLLFNCTL1_PD_Msk;
+
+    /* Set PLLFN frequency */
+    CLK->PLLFNCTL0 = (CLK->PLLFNCTL0 & ~(0x0FFFFFFFUL)) | 0x00000417UL;
+
+    /* Disable PLLFN first to avoid unstable when setting PLLFN */
+    CLK->PLLCTL |= CLK_PLLFNCTL1_PD_Msk;
+
+    /* Set PLLFN frequency */
+    CLK->PLLFNCTL1 = (CLK->PLLFNCTL1 & ~(0xF8000000UL)) | 0x00000000UL;
+
+    /* Set HCLK clock */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_PLL, CLK_CLKDIV0_HCLK(1));
+
+    /* Set PCLK-related clock */
+    CLK->PCLKDIV = (CLK_PCLKDIV_PCLK0DIV2 | CLK_PCLKDIV_PCLK1DIV2);
+
+    /* Enable IP clock */
+    CLK_EnableModuleClock(EMAC0_MODULE);
+    CLK_EnableModuleClock(FMCIDLE_MODULE);
+    CLK_EnableModuleClock(GPA_MODULE);
+    CLK_EnableModuleClock(GPB_MODULE);
+    CLK_EnableModuleClock(GPC_MODULE);
+    CLK_EnableModuleClock(GPD_MODULE);
+    CLK_EnableModuleClock(GPE_MODULE);
+    CLK_EnableModuleClock(PDMA0_MODULE);
+    CLK_EnableModuleClock(PDMA1_MODULE);
+    CLK_EnableModuleClock(RTC_MODULE);
+    CLK_EnableModuleClock(SPI0_MODULE);
+    CLK_EnableModuleClock(SPI1_MODULE);
+    CLK_EnableModuleClock(SPI2_MODULE);
+    CLK_EnableModuleClock(SPI3_MODULE);
+    CLK_EnableModuleClock(TMR0_MODULE);
+    CLK_EnableModuleClock(TMR1_MODULE);
+    CLK_EnableModuleClock(TMR2_MODULE);
+    CLK_EnableModuleClock(TMR3_MODULE);
+    CLK_EnableModuleClock(TRNG_MODULE);
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(UART1_MODULE);
+    CLK_EnableModuleClock(UART2_MODULE);
+    CLK_EnableModuleClock(UART3_MODULE);
+    CLK_EnableModuleClock(WDT_MODULE);
+    CLK_EnableModuleClock(WWDT_MODULE);
+
+    CLK_EnableCKO(CLK_CLKSEL1_CLKOSEL_PLLFN_DIV2, 1, 0);
+    CLK->CLKOCTL = CLK->CLKOCTL & ~CLK_CLKOCTL_CLK1HZEN_Msk;
+    CLK_EnableSysTick(CLK_CLKSEL0_STCLKSEL_HCLK_DIV2, 0);
+
+    /* Set IP clock */
+    CLK_SetModuleClock(RTC_MODULE, RTC_LXTCTL_RTCCKSEL_LIRC, MODULE_NoMsk);
+//    CLK_SetModuleClock(SPI0_MODULE, CLK_CLKSEL2_SPI0SEL_PCLK1, CLK_SPI0_CLKDIV_SPI0(1));
+//    CLK_SetModuleClock(SPI1_MODULE, CLK_CLKSEL2_SPI1SEL_PCLK0, CLK_SPI1_CLKDIV_SPI1(1));
+//    CLK_SetModuleClock(SPI2_MODULE, CLK_CLKSEL3_SPI2SEL_PCLK1, CLK_SPI2_CLKDIV_SPI2(1));
+//    CLK_SetModuleClock(SPI3_MODULE, CLK_CLKSEL3_SPI3SEL_PCLK0, CLK_SPI3_CLKDIV_SPI3(1));
+    CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0SEL_PCLK0, MODULE_NoMsk);
+    CLK_SetModuleClock(TMR1_MODULE, CLK_CLKSEL1_TMR1SEL_PCLK0, MODULE_NoMsk);
+    CLK_SetModuleClock(TMR2_MODULE, CLK_CLKSEL1_TMR2SEL_PCLK1, MODULE_NoMsk);
+    CLK_SetModuleClock(TMR3_MODULE, CLK_CLKSEL1_TMR3SEL_PCLK1, MODULE_NoMsk);
+    CLK_SetModuleClock(TRNG_MODULE, CLK_CLKSEL2_TRNGSEL_LIRC, MODULE_NoMsk);
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+    CLK_SetModuleClock(UART1_MODULE, CLK_CLKSEL1_UART1SEL_HXT, CLK_CLKDIV0_UART1(1));
+    CLK_SetModuleClock(UART2_MODULE, CLK_CLKSEL3_UART2SEL_HXT, CLK_CLKDIV4_UART2(1));
+    CLK_SetModuleClock(UART3_MODULE, CLK_CLKSEL3_UART3SEL_HXT, CLK_CLKDIV4_UART3(1));
+    CLK_SetModuleClock(WDT_MODULE, CLK_CLKSEL1_WDTSEL_LIRC, MODULE_NoMsk);
+    CLK_SetModuleClock(WWDT_MODULE, CLK_CLKSEL1_WWDTSEL_HCLK_DIV2048, MODULE_NoMsk);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+    // CLKO -> 25Mhz, PB14 JP7.33
+    SYS->GPB_MFP3 = SYS_GPB_MFP3_PB14MFP_CLKO;
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
 void SYS_Init(void)
 {
 
@@ -36,8 +139,8 @@ void SYS_Init(void)
     /* Wait for HIRC clock ready */
     CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
 
-    /* Set core clock to 192MHz */
-    CLK_SetCoreClock(192000000);
+    /* Set core clock to 120MHz */
+    CLK_SetCoreClock(120000000);
 
     /* Enable UART0 module clock */
     CLK_EnableModuleClock(UART0_MODULE);
@@ -113,11 +216,17 @@ void tx_application_define(void *first_unused_memory)
 
 int main()
 {
+    // Early bringup
+
     SYS_UnlockReg();
 
     SYS_Init();
 
     UART0_Init();
-    
+
+    // Application startup
+
+    SYS_Lightkraken_Init();
+
     tx_kernel_enter();
 }
